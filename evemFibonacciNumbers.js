2cd2c0b38f3afc64f232d522e498259f1ebe9b2d
SeungJe Jun <ninanung@naver.com>
2017-09-11 10:23:23 +0900

    solve even Fibonacci numbers

---
+++ evemFibonacciNumbers.js
@@ -0,0 +1,15 @@
+function evenFibonacciNumbers () {
+  var first = 1;
+	var second = 2;
+	var result = 0;
+	var sumOfEven = 2;
+	while(result < 2000000) {
+		result = first + second;
+		first = second;
+		second = result;
+		if (second % 2 == 0) {
+			sumOfEven += second;
+		}
+  }
+	return sumOfEven;
+}
