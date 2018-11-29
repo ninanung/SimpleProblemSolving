c9fee1eaa82d28a42dc49b33233c1cc169c0bc31
Seungje Jun <ninanung@naver.com>
2018-11-29 17:20:31 +0900

    new

--- Largest-product-in-a-grid.py
+++ Largest-product-in-a-grid.py
@@ -12,10 +12,26 @@ def getLinesReturnArray(lines):
     return xy
 
 def biggestUpDown(xy):
-    return None
+    xxxx = 0
+    for numX in range(0, len(xy[0])):
+        for numY in range(0, len(xy)-3):
+            if xxxx is 0:
+                xxxx = xy[numY][numX] * xy[numY+1][numX] * xy[numY+2][numX] * xy[numY+3][numX]
+            all = xy[numY][numX] * xy[numY+1][numX] * xy[numY+2][numX] * xy[numY+3][numX]
+            if xxxx < all:
+                xxxx = all
+    return xxxx
 
 def biggestLeftRight(xy):
-    return None
+    xxxx = 0
+    for numY in range(0, len(xy)):
+        for numX in range(0, len(xy[0])-3):
+            if xxxx is 0:
+                xxxx = xy[numY][numX] * xy[numY][numX+1] * xy[numY][numX+2] * xy[numY][numX+3]
+            all = xy[numY][numX] * xy[numY][numX+1] * xy[numY][numX+2] * xy[numY][numX+3]
+            if xxxx < all:
+                xxxx = all
+    return xxxx
 
 def biggestDiagonally(xy):
     return None
@@ -24,3 +40,5 @@ grid = open('grid.txt', 'rt')
 gridLines = grid.readlines()
 grid.close()
 xy = getLinesReturnArray(gridLines)
+print(biggestUpDown(xy))
+print(biggestLeftRight(xy))
